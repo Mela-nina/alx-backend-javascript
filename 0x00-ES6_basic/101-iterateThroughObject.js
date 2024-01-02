96a76ee50c336b3080cac7a26a68c4c6b1ecec8c
Mela-nina <oge4sym@gmail.com>
2024-01-02 20:04:41 +0000

    Task 14

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,14 +1,5 @@
-/* eslint-disable */
 export default function iterateThroughObject(reportWithIterator) {
-   let itemstr = '', j = 0;
-   for (const item of reportWithIterator) {
-        if (reportWithIterator.length - 1 == j) {
-            itemstr += item
-        }
-        else {
-            itemstr += item + ' | ';
-        }
-        j++;
-   }
-   return itemstr;
+  if (!reportWithIterator) {
+    console.log('Placeholder');
+  }
 }
